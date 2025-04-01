9625f13aa94e9faa746abbd1d40a95785c2f8402
dudosyka <a.hatson22@gmail.com>
2025-04-01 13:15:47 +0300

    News update fix

--- src/main/kotlin/philarmonic/modules/news/data/dto/UpdateNewsDto.kt
+++ src/main/kotlin/philarmonic/modules/news/data/dto/UpdateNewsDto.kt
@@ -5,13 +5,12 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class UpdateNewsDto(
-    
-    val date:Long?,
-    val name:String?,
-    val image:String?,
-    val imageName:String?,
-    val shortDescription:String?,
-    val description:String?,
-    val visible:Boolean?,
-    val position:Int?,
+    val date:Long? = null,
+    val name:String? = null,
+    val image:String? = null,
+    val imageName:String? = null,
+    val shortDescription:String? = null,
+    val description:String? = null,
+    val visible:Boolean? = null,
+    val position:Int? = null,
 )
